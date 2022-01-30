fb376861f7ae4e4d03ab14f36f199af28d0c064c
Jean-Paul <jpzambrano.leon@gmail.com>
2022-01-30 00:30:55 -0600

    updated file

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,55 +3,53 @@ import { alpha, AppBar, Avatar, Badge, Box, InputBase, Toolbar, Typography } fro
 import { createStyles, makeStyles, styled } from "@mui/styles";
 import { useState } from "react";
 
-const LogoLg = styled('div')(({ theme }) => ({
-    display: "none",
-    [theme.breakpoints.up("sm")]: {
-        display: "block"
-    }
-}));
-
-const LogoSm = styled('div')(({ theme }) => ({
-    display: "block",
-    [theme.breakpoints.up("sm")]: {
-        display: "none"
-    }
-}));
-
-const SearchBox = styled('div')(({ theme }) => ({
-    display: "flex",
-    alignItems: "center",
-    backgroundColor: alpha("#e9ecef", 1),
-    '&:hover': {
-        backgroundColor: alpha("#e9ecef", 0.75),
+const useStyles = makeStyles((theme) => ({
+    logoLg: {
+        display: "none",
+        [theme.breakpoints.up("sm")]: {
+            display: "block",
+        },
     },
-    borderRadius: "2rem",
-    width: "50%",
-    [theme.breakpoints.down("sm")]: {
-        display: (props) => (props.open ? "flex" : "none"),
-        width: "70%",
+    logoSm:{
+        display: "block",
+        [theme.breakpoints.up("sm")]: {
+            display: "none",
+        },
+    },
+    search: {
+        display: "flex",
+        alignItems: "center",
+        backgroundColor: alpha("#e9ecef", 1),
+        '&:hover': {
+            backgroundColor: alpha("#e9ecef", 0.75),
+        },
+        borderRadius: "2rem",
+        width: "50%",
+        [theme.breakpoints.down("sm")]: {
+            display: (props) => (props.open ? "flex" : "none"),
+            width: "70%",
+        },
+    },
+    cancel: {
+        [theme.breakpoints.up("sm")]: {
+            display: "none",
+        }
+    },
+    searchButton: {
+        marginRight: theme.spacing(2),
+        [theme.breakpoints.up("sm")]: {
+            display: "none"
+        }
+    },
+    icons: {
+        alignItems: "center",
+        display: (props) => (props.open ? "none" : "flex")
     }
-}));
-
-const CancelButton = styled('div')(({ theme }) => ({
-    [theme.breakpoints.up("sm")]: {
-        display: "none"
-    }
-}));
-
-const ButtonIcons = styled('div')({
-    alignItems: "center",
-    display: (props) => (props.open ? "none" : "flex")
-});
-
-const SearchButton = styled('div')(({ theme }) => ({
-    [theme.breakpoints.up("sm")]: {
-        display: "none"
-    }
-}));
+}))
 
 const Navbar = () => {
     const [open, setOpen] = useState(false);
-
+    const classes = useStyles({ open });
     return (
         <AppBar>
             <Toolbar 
@@ -59,35 +57,27 @@ const Navbar = () => {
                     display: "flex",
                     justifyContent: "space-between", 
                 }}>
-                <LogoLg>
-                    <Typography variant="h6">
-                        Project-Neo
-                    </Typography>
-                </LogoLg>
-                <LogoSm>
-                    <Typography variant="h6" >
-                        NEO
-                    </Typography>
-                </LogoSm>
-                <SearchBox>
+                <Typography variant="h6" className={classes.logoLg}>
+                    Project-Neo
+                </Typography>
+                <Typography variant="h6" className={classes.logoSm}>
+                    NEO
+                </Typography>
+                <div className={classes.search}>
                     <Search />
                     <InputBase placeholder="search" sx={{ ml: 1, width: "50vw", fontSize: "0.9rem"}}/>
-                    <CancelButton>
-                        <Cancel onClick={() => setOpen(false)} />
-                    </CancelButton>
-                </SearchBox>
-                <ButtonIcons>
-                    <SearchButton>
-                        <Search sx={{ mr: 2 }} onClick={() => setOpen(true)}/>
-                    </SearchButton>
+                    <Cancel className={classes.cancel} onClick={() => setOpen(false)} />
+                </div>
+                <div className={classes.icons}>
+                    <Search className={classes.searchButton} onClick={() => setOpen(true)}/>
                     <Badge sx={{ mr: 2 }}>
                         <MailOutlined />
                     </Badge>
-                    <Badge sx={{mr: 2}}>
+                    <Badge sx={{ mr: 2 }}>
                         <NotificationsOutlined />
                     </Badge>
                     <Avatar alt="Diana Ayi" />
-                </ButtonIcons>
+                </div>
             </Toolbar>
         </AppBar>
     )
