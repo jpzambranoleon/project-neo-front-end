25812fb756326c7971cdd844a0c864e7230cd070
Jean-Paul <jpzambrano.leon@gmail.com>
2022-01-31 16:01:05 -0600

    created Leftbar.jsx

---
+++ src/pages/profilePage/components/Leftbar.jsx
@@ -0,0 +1,11 @@
+import { Box } from "@mui/material"
+
+const Leftbar = () => {
+    return (
+        <Box sx={{ paddingTop: 10 }}>
+            <div>Left</div>
+        </Box>
+    );
+};
+
+export default Leftbar;
