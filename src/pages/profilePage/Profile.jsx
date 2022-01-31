4cdbcae2b6ea919924897f47143d6c0ef32ece3c
Jean-Paul <jpzambrano.leon@gmail.com>
2022-01-31 16:04:29 -0600

    updated files

--- src/pages/profilePage/Profile.jsx
+++ src/pages/profilePage/Profile.jsx
@@ -1,5 +1,7 @@
 import { Box, Grid } from "@mui/material";
 import Navbar from "../../components/Navbar";
+import Leftbar from "./components/Leftbar";
+import Rightbar from "./components/Rightbar";
 
 
 export default function Profile() {
@@ -9,10 +11,10 @@ export default function Profile() {
             <Box sx={{ gridTemplateColumns: "18vw auto 20vw", columnGap: "2rem", position: "relative" }}>
                 <Grid container sx={{ width: "80%", margin: "0 auto"}}>
                     <Grid item sm={8}>
-                        <div>Left</div>
+                        <Leftbar />
                     </Grid>
                     <Grid item sm={4}>
-                        <div>Right</div>
+                        <Rightbar />
                     </Grid>
                 </Grid>
             </Box>
---
+++ src/pages/profilePage/components/Rightbar.jsx
@@ -0,0 +1,11 @@
+import { Box } from "@mui/material"
+
+const Rightbar = () => {
+    return (
+        <Box sx={{ paddingTop: 10 }}>
+            <div>Right</div>
+        </Box>
+    );
+};
+
+export default Rightbar;
