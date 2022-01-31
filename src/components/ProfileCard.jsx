b80585506de8ff0a11ff88d66a7a4604821fd418
Jean-Paul <jpzambrano.leon@gmail.com>
2022-01-31 14:49:21 -0600

    updated ProfileCard.jsx

--- src/components/ProfileCard.jsx
+++ src/components/ProfileCard.jsx
@@ -1,4 +1,4 @@
-import { Card, CardMedia } from "@mui/material";
+import { Avatar, Button, Card, CardActions, CardContent, CardMedia, Typography } from "@mui/material";
 import { makeStyles } from "@mui/styles";
 
 const useStyles = makeStyles((theme) => ({
@@ -18,6 +18,32 @@ const ProfileCard = () => {
                 height="140"
                 image="/assets/cover/cover.jpg"
             />
+            <CardContent>
+                <Avatar alt="Diana Ayi" src="/assets/person/diana.jpg" />
+                <Typography variant="h5">
+                    Diana Ayi
+                    <Typography variant="h6" sx={{ color: "grey" }}>
+                        @dayi
+                    </Typography>
+                </Typography>
+            </CardContent>
+            <CardContent>
+                <Typography variant="body2">
+                    User Interface Designer and
+                    Front-End Developer
+                </Typography>
+            </CardContent>
+            <CardActions>
+                <Button size="small">
+                    Button 1
+                </Button>
+                <Button size="small">
+                    Button 2
+                </Button>
+                <Button size="small">
+                    Button 3
+                </Button>
+            </CardActions>
         </Card>
     );
 };
