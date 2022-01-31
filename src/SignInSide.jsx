2fe47ef8a4a401f6820eab05059f0d2efb65e722
Jean-Paul <jpzambrano.leon@gmail.com>
2022-01-31 14:35:50 -0600

    updated SignInSide.jsx

--- src/SignInSide.jsx
+++ src/SignInSide.jsx
@@ -37,7 +37,7 @@ export default function SignInSide() {
                     sm={4}
                     md={7}
                     sx={{
-                        backgroundImage: 'url(https://source.unsplash.com/random)',
+                        backgroundImage: 'url(/assets/background/loginBackground.jpg)',
                         backgroundRepeat: 'no-repeat',
                         backgroundColor: (t) =>
                             t.palette.mode === 'light' ? t.palette.grey[50] : t.palette.grey[900],
