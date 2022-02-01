ef7dab45405e0f35406168287f45bc6fa31c015c
Jean-Paul <jpzambrano.leon@gmail.com>
2022-02-01 00:37:30 -0600

    updated User.jsx

--- src/pages/profilePage/components/User.jsx
+++ src/pages/profilePage/components/User.jsx
@@ -1,51 +1,55 @@
-import { Avatar, Button, Card, CardActions, CardContent, CardMedia, Typography } from "@mui/material";
+import { EditOutlined } from "@mui/icons-material";
+import { Avatar, Card, CardActions, CardContent, CardMedia, IconButton, Typography } from "@mui/material";
 import { grey } from "@mui/material/colors";
 import { makeStyles } from "@mui/styles";
 
 const useStyles = makeStyles((theme) => ({
-    card: {
+    cardContainer: {
         marginRight: theme.spacing(3),
+
         [theme.breakpoints.down("sm")]: {
             marginRight: theme.spacing(0),
         }
+    },
+    userContent: {
+        display: "flex",
+        alignItems: "center",
+        justifyContent: "space-between",
+        marginTop: theme.spacing(-9)
     },
 }))
 
 const User = () => {
     const classes = useStyles();
     return (
-        <Card className={classes.card}>
+        <Card className={classes.cardContainer}>
             <CardMedia 
                 component="img"
                 height="200"
                 image="/assets/cover/cover.jpg"
             />
-            <CardContent sx={{  }}>
-                <Avatar alt="Diana Ayi" src="/assets/person/diana.jpg" sx={{  }}/>
-                <Typography variant="h5">
+            <CardContent>
+                <div className={classes.userContent}>
+                    <Avatar  alt="Diana Ayi" src="/assets/person/diana.jpg" sx={{ width: 160, height: 160, top: -50 }}/>
+                    <CardActions>
+                        <IconButton>
+                            <EditOutlined />
+                        </IconButton>
+                    </CardActions>
+                </div>
+                <Typography variant="h4" sx={{ mt: -4 }}>
                     Diana Ayi
-                    <Typography variant="h6" sx={{ color: grey[500] }}>
+                    <Typography variant="h5" sx={{ color: grey[500] }}>
                         @dayi
                     </Typography>
                 </Typography>
             </CardContent>
-            <CardContent sx={{  }}>
+            <CardContent className={classes.cardContent}>
                 <Typography variant="body2">
                     User Interface Designer and
                     Front-End Developer
                 </Typography>
             </CardContent>
-            <CardActions>
-                <Button size="small">
-                    Button 1
-                </Button>
-                <Button size="small">
-                    Button 2
-                </Button>
-                <Button size="small">
-                    Button 3
-                </Button>
-            </CardActions>
         </Card>
     );
 };
