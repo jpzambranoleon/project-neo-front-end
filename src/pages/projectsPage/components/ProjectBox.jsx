f6a5705ac9c91bd2a65f4611fee4a79eec10ed84
Jean-Paul <jpzambrano.leon@gmail.com>
2022-02-01 19:16:02 -0600

    created ProjectBox.jsx

---
+++ src/pages/projectsPage/components/ProjectBox.jsx
@@ -0,0 +1,11 @@
+import { Box } from "@mui/material"
+
+const ProjectBox = () => {
+    return (
+        <Box sx={{ paddingTop: 10 }}>
+            <div>Hello There</div>
+        </Box>
+    );
+};
+
+export default ProjectBox;
