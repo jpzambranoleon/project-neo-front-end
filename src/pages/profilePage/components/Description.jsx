58f389592d642ad67a66ec94a96fd5f4bb5f7396
Jean-Paul <jpzambrano.leon@gmail.com>
2022-02-05 00:06:26 -0600

    updated file

--- src/pages/profilePage/components/Description.jsx
+++ src/pages/profilePage/components/Description.jsx
@@ -48,7 +48,13 @@ const Description = () => {
                                 About
                             </Typography>
                             <Typography variant="body2">
-                                Lorem ipsum dolor sit amet consectetur adipisicing elit. Id, dolorum quae aliquam explicabo a in tenetur at. Deserunt explicabo quis quaerat dolorem id iusto delectus, neque asperiores! Tenetur, facere culpa.
+                                Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+                                Id, dolorum quae aliquam explicabo a in tenetur at. 
+                                Deserunt explicabo quis quaerat dolorem id iusto delectus, neque asperiores! 
+                                Tenetur, facere culpa. Lorem ipsum dolor sit, amet consectetur adipisicing elit. 
+                                Voluptates asperiores magnam natus nisi officiis, 
+                                reprehenderit aperiam deserunt atque maxime at iusto, consequuntur esse, 
+                                ipsa fugit blanditiis praesentium laudantium rem distinctio?
                             </Typography>
                             </Container>
                         </Grid>
