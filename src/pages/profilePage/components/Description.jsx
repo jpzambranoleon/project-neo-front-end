09989a20282d3a993f18ba3214304553a27fea91
Jean-Paul <jpzambrano.leon@gmail.com>
2022-02-05 16:19:54 -0600

    updated file

--- src/pages/profilePage/components/Description.jsx
+++ src/pages/profilePage/components/Description.jsx
@@ -1,4 +1,5 @@
-import { Avatar, Box, Button, Grid, TextField, Typography, Container } from "@mui/material";
+import { Avatar, Box, Button, Grid, TextField, Typography, Container, CardContent } from "@mui/material";
+import { grey } from "@mui/material/colors";
 
 const Description = () => {
     return (
@@ -9,29 +10,28 @@ const Description = () => {
                     <Typography component="h1" variant="h4">
                         Diana Ayi
                     </Typography>
+                    <Typography color={grey[500]}>
+                        @dayi
+                    </Typography>
                     <Typography variant="body2">
                         User Interface Designer and Front-End Developer
                     </Typography>
                 </Container>
                 <Box sx={{ mt: 3 }}>
-                    <Grid container spacing={2}>
-                        <Grid item xs={12}>
-                            <Container>
-                            <Typography variant="h5">
-                                About
-                            </Typography>
-                            <Typography variant="body2">
-                                Lorem ipsum dolor sit amet consectetur adipisicing elit. 
-                                Id, dolorum quae aliquam explicabo a in tenetur at. 
-                                Deserunt explicabo quis quaerat dolorem id iusto delectus, neque asperiores! 
-                                Tenetur, facere culpa. Lorem ipsum dolor sit, amet consectetur adipisicing elit. 
-                                Voluptates asperiores magnam natus nisi officiis, 
-                                reprehenderit aperiam deserunt atque maxime at iusto, consequuntur esse, 
-                                ipsa fugit blanditiis praesentium laudantium rem distinctio?
-                            </Typography>
-                            </Container>
-                        </Grid>
-                    </Grid>
+                <Container>
+                    <Typography variant="h5">
+                        About
+                    </Typography>
+                    <Typography variant="body2">
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+                        Id, dolorum quae aliquam explicabo a in tenetur at. 
+                        Deserunt explicabo quis quaerat dolorem id iusto delectus, neque asperiores! 
+                        Tenetur, facere culpa. Lorem ipsum dolor sit, amet consectetur adipisicing elit. 
+                        Voluptates asperiores magnam natus nisi officiis, 
+                        reprehenderit aperiam deserunt atque maxime at iusto, consequuntur esse, 
+                        ipsa fugit blanditiis praesentium laudantium rem distinctio?
+                    </Typography>
+                </Container>
                 </Box>
             </Box>
         </Box>
