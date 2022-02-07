793a6b47db661137e4c27c129e410cdead5fd082
Jean-Paul <jpzambrano.leon@gmail.com>
2022-02-06 23:57:52 -0600

    created Sidebar2.jsx

---
+++ src/pages/homePage/components/Sidebar2.jsx
@@ -0,0 +1,74 @@
+import { Analytics, Bookmark, Chat, Dashboard, Drafts, Inbox, Person, Settings } from "@mui/icons-material";
+import { Box, Card, Divider, List, ListItemButton, ListItemIcon, ListItemText } from "@mui/material";
+import { useState } from "react";
+
+const Sidebar2 = () => {
+    const [selectedIndex, setSelectedIndex] = useState(1);
+
+    const handleListItemClick = (event, index) => {
+      setSelectedIndex(index);
+    };
+
+    return (
+        <Box sx={{ bgcolor: "white", mt: 3 }}>
+            <List component="nav" aria-label="main mailbox folders">
+                <ListItemButton
+                    selected={selectedIndex === 0}
+                    onClick={(event) => handleListItemClick(event, 0)}
+                >
+                    <ListItemIcon>
+                        <Dashboard htmlColor="purple"/>
+                    </ListItemIcon>
+                    <ListItemText primary="Dashboard" />
+                </ListItemButton>
+                <ListItemButton
+                    selected={selectedIndex === 1}
+                    onClick={(event) => handleListItemClick(event, 1)}
+                >
+                    <ListItemIcon>
+                        <Person htmlColor="goldenrod"/>
+                    </ListItemIcon>
+                    <ListItemText primary="User" />
+                </ListItemButton>
+                <ListItemButton
+                    selected={selectedIndex === 2}
+                    onClick={(event) => handleListItemClick(event, 2)}
+                >
+                    <ListItemIcon>
+                        <Chat htmlColor="green"/>
+                    </ListItemIcon>
+                    <ListItemText primary="Chat" />
+                </ListItemButton>
+                <ListItemButton
+                    selected={selectedIndex === 3}
+                    onClick={(event) => handleListItemClick(event, 3)}
+                >
+                    <ListItemIcon>
+                        <Analytics htmlColor="tomato"/>
+                    </ListItemIcon>
+                    <ListItemText primary="Analytics" />
+                </ListItemButton>
+                <ListItemButton
+                    selected={selectedIndex === 4}
+                    onClick={(event) => handleListItemClick(event, 4)}
+                >
+                    <ListItemIcon>
+                        <Bookmark htmlColor="blue"/>
+                    </ListItemIcon>
+                    <ListItemText primary="Bookmark" />
+                </ListItemButton>
+                <ListItemButton
+                    selected={selectedIndex === 5}
+                    onClick={(event) => handleListItemClick(event, 5)}
+                >
+                    <ListItemIcon>
+                        <Settings htmlColor="grey"/>
+                    </ListItemIcon>
+                    <ListItemText primary="Settings" />
+                </ListItemButton>
+            </List>
+        </Box>
+    )
+};
+
+export default Sidebar2;
