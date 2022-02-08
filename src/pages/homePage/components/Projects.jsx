9f9276b265c0f98dd3ea946fce205fda3061d813
Jean-Paul <jpzambrano.leon@gmail.com>
2022-02-07 20:11:05 -0600

    updated Projects.jsx

--- src/pages/homePage/components/Projects.jsx
+++ src/pages/homePage/components/Projects.jsx
@@ -1,4 +1,5 @@
-import { Avatar, Box, CardContent, CardHeader, Container, ListItem, ListItemAvatar, ListItemButton, Typography } from "@mui/material";
+import { MoreVert } from "@mui/icons-material";
+import { Avatar, Box, CardContent, CardHeader, CardMedia, Container, Divider, IconButton, ListItem, ListItemAvatar, ListItemButton, Typography } from "@mui/material";
 
 const Projects = () => {
     return (
@@ -8,6 +9,30 @@ const Projects = () => {
                     Interesting Projects
                 </Typography>
             </ListItem>
+            <Divider />
+            <CardHeader 
+                avatar={
+                    <Avatar src="/assets/person/diana.jpg"/>
+                }
+                title="Project-NEO"
+                subheader="5 hours ago"
+                action={
+                    <IconButton>
+                        <MoreVert />
+                    </IconButton>
+                }
+            />
+            <CardMedia 
+                component="img"
+                height="140"
+                image="https://pi.tedcdn.com/r/talkstar-photos.s3.amazonaws.com/uploads/72bda89f-9bbf-4685-910a-2f151c4f3a8a/NicolaSturgeon_2019T-embed.jpg?w=512"
+                alt="Nicola Sturgeon on a TED talk stage"
+            />
+            <CardContent>
+                <Typography variant="body2" color="text.secondary" component="p">
+                    Why First Minister of Scotland Nicola Sturgeon thinks GDP is the wrong measure of a country's success:
+                </Typography>
+            </CardContent>
         </Box>
     );
 };
