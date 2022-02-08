d112cd97823d26008268f8a69591b3a3ff88e074
Jean-Paul <jpzambrano.leon@gmail.com>
2022-02-07 19:48:12 -0600

    updated Projects.jsx

--- src/pages/homePage/components/Projects.jsx
+++ src/pages/homePage/components/Projects.jsx
@@ -2,36 +2,12 @@ import { Avatar, Box, CardContent, CardHeader, Container, ListItem, ListItemAvat
 
 const Projects = () => {
     return (
-        <Box sx={{ bgcolor: "white" }}>
+        <Box sx={{ bgcolor: "white", mt: 3 }}>
             <ListItem>
                 <Typography variant="h2" sx={{ fontWeight: "700", fontSize: "20px" }}>
-                    Who to follow
+                    Interesting Projects
                 </Typography>
             </ListItem>
-            <ListItemButton>
-                <ListItemAvatar>
-                    <Avatar src="https://mui.com/static/images/avatar/1.jpg"/>
-                </ListItemAvatar>
-                <Typography sx={{ fontWeight: "500" }}>
-                    Remy Sharp
-                </Typography>
-            </ListItemButton>
-            <ListItemButton>
-                <ListItemAvatar>
-                    <Avatar src="https://mui.com/static/images/avatar/2.jpg"/>
-                </ListItemAvatar>
-                <Typography sx={{ fontWeight: "500" }}>
-                    Travis Howard
-                </Typography>
-            </ListItemButton>
-            <ListItemButton>
-                <ListItemAvatar>
-                    <Avatar src="https://mui.com/static/images/avatar/3.jpg"/>
-                </ListItemAvatar>
-                <Typography sx={{ fontWeight: "500" }}>
-                    Cindy Baker
-                </Typography>
-            </ListItemButton>
         </Box>
     );
 };
