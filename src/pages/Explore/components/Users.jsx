69378a51e061297c9b7b437a78fb89ab33e5f8cc
Jean-Paul <jpzambrano.leon@gmail.com>
2022-02-09 15:55:09 -0600

    created Users.jsx

---
+++ src/pages/Explore/components/Users.jsx
@@ -0,0 +1,19 @@
+import { Box, Card, CardContent, Grid, ListItem, Typography } from "@mui/material";
+
+const Skills = () => {
+    return (
+        <Box sx={{ mt: 3, bgcolor: "white" }}>
+            <Grid container>
+                <Grid item sm={2}>
+                    <Card>
+                        <Typography>
+                            Hello
+                        </Typography>
+                    </Card>
+                </Grid>
+            </Grid>
+        </Box>
+    );
+};
+
+export default Skills;
