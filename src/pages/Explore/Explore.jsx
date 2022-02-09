d4e1c88de45cff02b4665cae7658c024bb3d878e
Jean-Paul <jpzambrano.leon@gmail.com>
2022-02-09 15:43:34 -0600

    created Explore.jsx

---
+++ src/pages/Explore/Explore.jsx
@@ -0,0 +1,42 @@
+import { Box, Grid, Paper, TextField } from "@mui/material";
+import Navbar from "../../components/Navbar";
+import { makeStyles } from "@mui/styles";
+import Sidebar from "../homePage/components/Sidebar";
+
+const useStyles = makeStyles((theme) => ({
+    boxContainer: {
+        width: "80%",
+        margin:"0 auto",
+        marginTop: theme.spacing(10),
+
+        [theme.breakpoints.down("sm")]: {
+            width: "100%"
+        }
+    },
+}))
+
+export default function Explore() {
+    const classes = useStyles();
+    return (
+        <div>
+            <Navbar />
+            <Box className={classes.boxContainer}>
+                <Grid container spacing={3}>
+                    <Grid item sm={3}>
+                        <Box>
+                            <UserCard />
+                            <Sidebar />
+                        </Box>
+                    </Grid>
+                    <Grid item sm={6}>
+                        <Description />
+                        <Skills />
+                    </Grid>
+                    <Grid item sm={3}>
+                        <Friends />
+                    </Grid>
+                </Grid>
+            </Box>
+        </div>
+    );
+}
