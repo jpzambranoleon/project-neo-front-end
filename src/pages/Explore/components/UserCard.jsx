1a21a83f7c8691626efcbfdb03d04e1a7d16b875
Jean-Paul <jpzambrano.leon@gmail.com>
2022-02-09 21:18:47 -0600

    updated files

--- src/pages/Explore/components/UserCard.jsx
+++ src/pages/Explore/components/UserCard.jsx
@@ -14,14 +14,16 @@ const UserCard = () => {
                 <Typography variant="h2" sx={{ fontWeight: "500", fontSize: "20px" }}>
                     Diana Ayi
                 </Typography>
-                <ListItem>
+                <CardContent>
                     <Typography variant="body2" color="text.secondary">
                             User Interface Designer and Front-End Developer
                     </Typography>
-                </ListItem>
-                <Button variant="outlined" fullWidth>
-                    Recruit
-                </Button>
+                </CardContent>
+                <CardContent>
+                    <Button variant="outlined" sx={{ width: "200px" }}>
+                        Recruit
+                    </Button>
+                </CardContent>
             </Grid>
         </Card>
     )
