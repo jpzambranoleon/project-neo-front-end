2ca8be294df06035478fc277b0bbec31c2c8eace
Jean-Paul <jpzambrano.leon@gmail.com>
2022-02-09 20:15:48 -0600

    updated files

--- src/pages/Explore/components/UserCard.jsx
+++ src/pages/Explore/components/UserCard.jsx
@@ -1,60 +1,17 @@
-import { GitHub, Google, LinkedIn } from "@mui/icons-material";
-import { Avatar, Box, Button, ButtonGroup, Card, CardActionArea, CardContent, Grid, IconButton, Stack, Typography } from "@mui/material";
+import { Add, GitHub, Google, LinkedIn, Send } from "@mui/icons-material";
+import { Avatar, Box, Button, ButtonGroup, Card, CardActionArea, CardContent, Grid, IconButton, ListItem, Stack, Typography } from "@mui/material";
 
-const Card = () => {
+const UserCard = () => {
     return(
         <Card>
-            <CardContent>
-                <Grid container direction="column" sx={{ alignItems: "center" }}>
-                    <Avatar sx={{ width: 120, height: 120 }} src="/assets/person/diana.jpg"/>
-                    <CardContent>
-                        <Typography variant="h5">
-                            Diana Ayi
-                        </Typography>
-                    </CardContent>
-                    <Typography variant="body2">
-                        User Interface Designer and Front-End Developer
-                    </Typography>
-                    <Box sx={{ display: "flex" }}>
-                        <IconButton size="small">
-                            <LinkedIn fontSize="small"/>
-                        </IconButton>
-                        <IconButton size="small">
-                            <Google fontSize="small"/>
-                        </IconButton>
-                        <IconButton size="small">
-                            <GitHub fontSize="small"/>
-                        </IconButton>
-                    </Box>
-                    <CardContent>
-                        <Stack spacing={2} direction="row">
-                            <Button size="small" variant="contained">
-                                <Send fontSize="small" />
-                                Message
-                            </Button>
-                            <Button size="small" variant="contained">
-                                <Add fontSize="small" />
-                                Recruit
-                            </Button>
-                        </Stack>
-                    </CardContent>
-                    <Box>
-                        <ButtonGroup variant="text" size="small">
-                            <Button>
-                                Button 1
-                            </Button>
-                            <Button>
-                                Button 2
-                            </Button>
-                            <Button>
-                                Button 3
-                            </Button>
-                        </ButtonGroup>
-                    </Box>
-                </Grid>
-            </CardContent>
+            <Grid container direction="column" sx={{ alignItems: "center" }}>
+                <Avatar src="/assets/person/diana.jpg" />
+                <Typography>
+                    Diana Ayi
+                </Typography>
+            </Grid>
         </Card>
     )
 }
 
-export default Card;
+export default UserCard;
--- src/pages/Explore/components/Users.jsx
+++ src/pages/Explore/components/Users.jsx
@@ -1,62 +1,13 @@
 import { Add, GitHub, Google, LinkedIn, Send } from "@mui/icons-material";
 import { Avatar, Box, Button, ButtonGroup, Card, CardContent, Grid, IconButton, ListItem, Stack, Typography } from "@mui/material";
+import UserCard from "./UserCard";
 
 const Users = () => {
     return (
         <Box sx={{ bgcolor: "white" }}>
             <Grid container>
                 <Grid item sm={3}>
-                    <Card>
-                        <CardContent>
-                            <Grid container direction="column" sx={{ alignItems: "center" }}>
-                                <Avatar sx={{ width: 120, height: 120 }} src="/assets/person/diana.jpg"/>
-                                <CardContent>
-                                    <Typography variant="h5">
-                                        Diana Ayi
-                                    </Typography>
-                                </CardContent>
-                                <Typography variant="body2">
-                                    User Interface Designer and Front-End Developer
-                                </Typography>
-                                <Box sx={{ display: "flex" }}>
-                                    <IconButton size="small">
-                                        <LinkedIn fontSize="small"/>
-                                    </IconButton>
-                                    <IconButton size="small">
-                                        <Google fontSize="small"/>
-                                    </IconButton>
-                                    <IconButton size="small">
-                                        <GitHub fontSize="small"/>
-                                    </IconButton>
-                                </Box>
-                                <CardContent>
-                                    <Stack spacing={2} direction="row">
-                                        <Button size="small" variant="contained">
-                                            <Send fontSize="small" />
-                                            Message
-                                        </Button>
-                                        <Button size="small" variant="contained">
-                                            <Add fontSize="small" />
-                                            Recruit
-                                        </Button>
-                                    </Stack>
-                                </CardContent>
-                                <Box>
-                                    <ButtonGroup variant="text" size="small">
-                                        <Button>
-                                            Button 1
-                                        </Button>
-                                        <Button>
-                                            Button 2
-                                        </Button>
-                                        <Button>
-                                            Button 3
-                                        </Button>
-                                    </ButtonGroup>
-                                </Box>
-                            </Grid>
-                        </CardContent>
-                    </Card>
+                    <UserCard />
                 </Grid>
                 <Grid item sm={3}>
                     <Card>
