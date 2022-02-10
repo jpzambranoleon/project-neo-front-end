a4576cb4da722b7ad59d0d536210ec17aa71fe25
Jean-Paul <jpzambrano.leon@gmail.com>
2022-02-09 21:14:39 -0600

    updated files

--- src/pages/Explore/components/UserCard.jsx
+++ src/pages/Explore/components/UserCard.jsx
@@ -1,14 +1,27 @@
 import { Add, GitHub, Google, LinkedIn, Send } from "@mui/icons-material";
-import { Avatar, Box, Button, ButtonGroup, Card, CardActionArea, CardContent, Grid, IconButton, ListItem, Stack, Typography } from "@mui/material";
+import { Avatar, Box, Button, ButtonGroup, Card, CardActionArea, CardContent, CardMedia, Container, Grid, IconButton, ListItem, Stack, Typography } from "@mui/material";
 
 const UserCard = () => {
     return(
         <Card>
-            <Grid container direction="column" sx={{ alignItems: "center" }}>
-                <Avatar src="/assets/person/diana.jpg" />
-                <Typography>
+            <CardMedia
+                component="img"
+                src="/assets/cover/cover.jpg"
+                    
+            />
+            <Grid container direction="column" sx={{ alignItems: "center", mt: -8 }}>
+                <Avatar src="/assets/person/diana.jpg" sx={{ width: 110, height: 110 }} />
+                <Typography variant="h2" sx={{ fontWeight: "500", fontSize: "20px" }}>
                     Diana Ayi
                 </Typography>
+                <ListItem>
+                    <Typography variant="body2" color="text.secondary">
+                            User Interface Designer and Front-End Developer
+                    </Typography>
+                </ListItem>
+                <Button variant="outlined" fullWidth>
+                    Recruit
+                </Button>
             </Grid>
         </Card>
     )
