8b3732e890aabcb12a8b42ffc866fca65b24d871
Jean-Paul <jpzambrano.leon@gmail.com>
2022-02-09 18:02:42 -0600

    udpated files

--- src/pages/Explore/components/Users.jsx
+++ src/pages/Explore/components/Users.jsx
@@ -1,4 +1,5 @@
-import { Box, Card, CardContent, Grid, ListItem, Typography } from "@mui/material";
+import { Add, GitHub, Google, LinkedIn, Send } from "@mui/icons-material";
+import { Avatar, Box, Button, ButtonGroup, Card, CardContent, Grid, IconButton, ListItem, Stack, Typography } from "@mui/material";
 
 const Users = () => {
     return (
@@ -6,9 +7,55 @@ const Users = () => {
             <Grid container>
                 <Grid item sm={3}>
                     <Card>
-                        <Typography>
-                            Hello
-                        </Typography>
+                        <CardContent>
+                            <Grid container direction="column" sx={{ alignItems: "center" }}>
+                                <Avatar sx={{ width: 120, height: 120 }} src="/assets/person/diana.jpg"/>
+                                <CardContent>
+                                    <Typography variant="h5">
+                                        Diana Ayi
+                                    </Typography>
+                                </CardContent>
+                                <Typography variant="body2">
+                                    User Interface Designer and Front-End Developer
+                                </Typography>
+                                <Box sx={{ display: "flex" }}>
+                                    <IconButton size="small">
+                                        <LinkedIn fontSize="small"/>
+                                    </IconButton>
+                                    <IconButton size="small">
+                                        <Google fontSize="small"/>
+                                    </IconButton>
+                                    <IconButton size="small">
+                                        <GitHub fontSize="small"/>
+                                    </IconButton>
+                                </Box>
+                                <CardContent>
+                                    <Stack spacing={2} direction="row">
+                                        <Button size="small" variant="contained">
+                                            <Send fontSize="small" />
+                                            Message
+                                        </Button>
+                                        <Button size="small" variant="contained">
+                                            <Add fontSize="small" />
+                                            Recruit
+                                        </Button>
+                                    </Stack>
+                                </CardContent>
+                                <Box>
+                                    <ButtonGroup variant="text" size="small">
+                                        <Button>
+                                            Button 1
+                                        </Button>
+                                        <Button>
+                                            Button 2
+                                        </Button>
+                                        <Button>
+                                            Button 3
+                                        </Button>
+                                    </ButtonGroup>
+                                </Box>
+                            </Grid>
+                        </CardContent>
                     </Card>
                 </Grid>
                 <Grid item sm={3}>
