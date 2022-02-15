6d0d517dc2ccf43220d1c5f3d948cee3afad924c
Jean-Paul <jpzambrano.leon@gmail.com>
2022-02-14 23:30:14 -0600

    updated SearchBar.jsx

--- src/pages/Explore/components/SearchBar.jsx
+++ src/pages/Explore/components/SearchBar.jsx
@@ -1,11 +1,19 @@
-import { Menu } from "@mui/icons-material";
-import { IconButton, Paper } from "@mui/material";
+import { Menu, Search } from "@mui/icons-material";
+import { IconButton, InputBase, Paper } from "@mui/material";
 
 const SearchBar = () => {
     return (
         <Paper component="form" >
             <IconButton>
                 <Menu />
+            </IconButton>
+            <InputBase 
+                sx={{ ml: 1, flex: 1 }}
+                placeholder="Search"
+                inputProps={{ 'aria-label': 'search' }}
+            />
+             <IconButton type="submit" sx={{ p: '10px' }} aria-label="search">
+                <Search />
             </IconButton>
         </Paper>
     );
