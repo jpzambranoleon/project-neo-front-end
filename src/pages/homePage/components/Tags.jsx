0f4a5c7c9557825d87b2d8474c64d53a15a36cab
Jean-Paul <jpzambrano.leon@gmail.com>
2022-02-20 17:52:35 -0600

    updated Tags.jsx

---
+++ src/pages/homePage/components/Tags.jsx
@@ -0,0 +1,24 @@
+import { Paper } from "@mui/material";
+
+const Tags = () => {
+    return (
+        <Paper>
+            <Paper>
+                <Typography gutterBottom>
+                    Categories
+                </Typography>
+                <Button variant="outlined" sx={{ mr: 2, color: '#555', fontSize: 15, textTransform: 'none' }}>
+                    JavaScript
+                </Button>
+                <Button variant="outlined" sx={{ mr: 2, color: '#555', fontSize: 15, textTransform: 'none' }}>
+                    CSS
+                </Button>
+                <Button variant="outlined" sx={{ mr: 2, color: '#555', fontSize: 15, textTransform: 'none' }}>
+                    React
+                </Button>
+            </Paper>
+        </Paper>
+    )
+}
+
+export default Tags;
