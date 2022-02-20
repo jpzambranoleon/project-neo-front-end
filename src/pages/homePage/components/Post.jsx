969b86bca6305cf83c13407e026d48aed2e39afc
Jean-Paul <jpzambrano.leon@gmail.com>
2022-02-19 19:35:37 -0600

    updated Post.jsx

--- src/pages/homePage/components/Post.jsx
+++ src/pages/homePage/components/Post.jsx
@@ -1,65 +1,13 @@
 import { Code, EmojiEmotions, Label, PermMedia,} from "@mui/icons-material";
-import { alpha, Button, InputBase, Typography, Paper, styled, TextField, Container, Box, CardContent, List, ListItem, Avatar } from "@mui/material";
-import { makeStyles } from "@mui/styles";
-
-const StyledTextField = styled(TextField)(({ theme }) => ({
-    width: '100%',
-    backgroundColor: alpha(theme.palette.common.white),
-    '&:hover': {
-        backgroundColor: alpha(theme.palette.common.black, 0.15),
-    },
-}));
-
-const SearchField = styled('div')(({ theme }) => ({
-    position: 'relative',
-    borderRadius: theme.shape.borderRadius,
-    backgroundColor: alpha(theme.palette.common.white),
-    '&:hover': {
-      backgroundColor: alpha(theme.palette.common.black, 0.15),
-    },
-    marginRight: theme.spacing(2),
-    marginLeft: 0,
-    width: '100%',
-}));
-
-const StyledInputBase = styled(InputBase)(({ theme }) => ({
-    color: 'inherit',
-    '& .MuiInputBase-input': {
-        padding: theme.spacing(1, 1, 1, 0),
-        // vertical padding + font size from searchIcon
-        paddingLeft: `calc(1em + ${theme.spacing(4)})`,
-        transition: theme.transitions.create('width'),
-        width: '100%',
-        [theme.breakpoints.up('md')]: {
-        width: '20ch',
-        },
-    },
-}));
-
-const useStyles = makeStyles((theme) => ({
-    inputBase: {
-        alignItems: "center",
-        padding: "0.4rem 1rem",
-        marginBottom: theme.spacing(2)
-    },
-    buttonGroup: {
-        display: "flex",
-        justifyContent: "space-between",
-        marginLeft: theme.spacing(1),
-        marginBottom: theme.spacing(2),
-    }
-}))
+import { alpha, Button, InputBase, Typography, Paper, Box, ListItem } from "@mui/material";
 
 const Post = () => {
-    const classes = useStyles();
     return (
         <Paper>
             <ListItem>
-            <Paper sx={{ width: '100%' }}>
-                <SearchField>
-                    <InputBase placeholder="What's on your mind?"/>
-                </SearchField>
-            </Paper>
+                <Button fullWidth variant="outlined">
+                    What's on your mind?
+                </Button>
             </ListItem>
             <ListItem sx={{ mb: 2, display: 'flex', justifyContent: 'space-between' }}>
                 <Button>
