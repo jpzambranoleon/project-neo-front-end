115cdea3b4410edd9196be07b9bf8ce01804071f
Jean-Paul <jpzambrano.leon@gmail.com>
2022-02-20 18:27:32 -0600

    updated Tags.jsx

--- src/pages/homePage/components/Tags.jsx
+++ src/pages/homePage/components/Tags.jsx
@@ -3,18 +3,27 @@ import { Box, Button, Paper, Typography } from "@mui/material";
 const Tags = () => {
     return (
         <Paper sx={{ mt: 3 }}>
+            <Typography gutterBottom>
+                Categories
+            </Typography>
             <Box sx={{ ml: 1 }}>
-                <Typography gutterBottom>
-                    Categories
-                </Typography>
-                <Button variant="outlined" sx={{ mr: 1, color: '#555', fontSize: 15, textTransform: 'none' }}>
+                <Button variant="outlined" sx={{ textTransform: 'none', mb: 1, mr: 1 }}>
                     JavaScript
                 </Button>
-                <Button variant="outlined" sx={{ mr: 1, color: '#555', fontSize: 15, textTransform: 'none' }}>
+                <Button variant="outlined" sx={{ textTransform: 'none', mb: 1, mr: 1 }}>
                     CSS
                 </Button>
-                <Button variant="outlined" sx={{ mr: 1, color: '#555', fontSize: 15, textTransform: 'none' }}>
+                <Button variant="outlined" sx={{ textTransform: 'none', mb: 1, mr: 1 }}>
                     React
+                </Button>
+                <Button variant="outlined" sx={{ textTransform: 'none', mb: 1, mr: 1 }}>
+                    Front-End Development
+                </Button>
+                <Button variant="outlined" sx={{ textTransform: 'none', mb: 1, mr: 1 }}>
+                    Project-Neo
+                </Button>
+                <Button variant="outlined" sx={{ textTransform: 'none', mb: 1, mr: 1 }}>
+                    Web3
                 </Button>
             </Box>
         </Paper>
