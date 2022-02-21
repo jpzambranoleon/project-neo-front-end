69d98f0e4e0f8bed46b44d385a130183aa49cda3
Jean-Paul <jpzambrano.leon@gmail.com>
2022-02-21 14:58:57 -0600

    updated News.jsx

--- src/pages/homePage/components/News.jsx
+++ src/pages/homePage/components/News.jsx
@@ -1,23 +1,28 @@
-import { Box, CardMedia, ListItemButton, Paper, Typography } from "@mui/material";
+import { Box, CardActionArea, CardContent, CardMedia, ListItemButton, Paper, Typography } from "@mui/material";
 
 const News = () => {
     return (
         <Paper sx={{ mb: 2 }}>
-            <Typography>
-                What Might Interest You
-            </Typography>
-            <ListItemButton>
-                <Typography sx={{ fontSize: 14 }} color="text.secondary" gutterBottom>
-                    News
-                </Typography>
-                <Box>
-                    <CardMedia 
-                        component="img"
-                        image="/assets/background/loginBackground.jpg"
-                        sx={{ maxHeight: 50 }}
-                    />
-                </Box>
-            </ListItemButton>
+            <CardActionArea sx={{ display: 'flex' }}>
+                <CardContent>
+                    <Typography sx={{ fontSize: 14 }} color="text.secondary">
+                        News
+                    </Typography>
+                    <Typography component="div">
+                        Project Neo goes live!
+                    </Typography>
+                    <Typography sx={{ fontSize: 12 }} color="text.secondary">
+                        12.0K Likes
+                    </Typography>
+                </CardContent>
+                <CardContent>
+                <CardMedia 
+                    component="img"
+                    src="/assets/background/loginBackground.jpg"
+                    sx={{ height: 50 }}
+                />
+                </CardContent>
+            </CardActionArea>
         </Paper>
     );
 };
