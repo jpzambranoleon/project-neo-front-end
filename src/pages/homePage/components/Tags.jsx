894fb677675ab0ab4da80eb9ef49a53e5378f476
Jean-Paul <jpzambrano.leon@gmail.com>
2022-02-20 18:00:29 -0600

    updated Tags.jsx

--- src/pages/homePage/components/Tags.jsx
+++ src/pages/homePage/components/Tags.jsx
@@ -1,20 +1,22 @@
-import { Button, Paper, Typography } from "@mui/material";
+import { Box, Button, Paper, Typography } from "@mui/material";
 
 const Tags = () => {
     return (
         <Paper>
-            <Typography gutterBottom>
-                Categories
-            </Typography>
-            <Button variant="outlined" sx={{ mr: 2, color: '#555', fontSize: 15, textTransform: 'none' }}>
-                JavaScript
-            </Button>
-            <Button variant="outlined" sx={{ mr: 2, color: '#555', fontSize: 15, textTransform: 'none' }}>
-                CSS
-            </Button>
-            <Button variant="outlined" sx={{ mr: 2, color: '#555', fontSize: 15, textTransform: 'none' }}>
-                React
-            </Button>
+            <Box sx={{ ml: 2 }}>
+                <Typography gutterBottom>
+                    Categories
+                </Typography>
+                <Button variant="outlined" sx={{ mr: 2, color: '#555', fontSize: 15, textTransform: 'none' }}>
+                    JavaScript
+                </Button>
+                <Button variant="outlined" sx={{ mr: 2, color: '#555', fontSize: 15, textTransform: 'none' }}>
+                    CSS
+                </Button>
+                <Button variant="outlined" sx={{ mr: 2, color: '#555', fontSize: 15, textTransform: 'none' }}>
+                    React
+                </Button>
+            </Box>
         </Paper>
     )
 }
