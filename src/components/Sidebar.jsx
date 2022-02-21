20da37801f4b2ef9951246b6a60a0dcd46d7ff0a
Jean-Paul <jpzambrano.leon@gmail.com>
2022-02-20 18:39:11 -0600

    updated Sidebar.jsx

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -3,7 +3,7 @@ import { ListItemButton, ListItemIcon, Paper, Typography } from "@mui/material";
 
 const Sidebar = () => {
     return (
-        <Paper sx={{ mt: 3 }}>
+        <Paper sx={{ mt: 2 }}>
             <ListItemButton sx={{ padding: "20px 20px" }}>
                 <ListItemIcon>
                     <Home />
