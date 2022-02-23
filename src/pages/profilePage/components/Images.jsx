9f7a6ac9554f0f49d90059bbbdd52e496f0bb7fc
Jean-Paul <jpzambrano.leon@gmail.com>
2022-02-23 17:51:21 -0600

    updated Images.jsx

--- src/pages/profilePage/components/Images.jsx
+++ src/pages/profilePage/components/Images.jsx
@@ -1,18 +1,8 @@
 import { ImageList, ImageListItem } from "@mui/material";
 
-function srcset(image, size, rows = 1, cols = 1) {
-    return {
-        src: '${image}?w=${size * cols}&h=${size * rows}&fit=crop&auto=format',
-        srcSet: `${image}?w=${size * cols}&h=${
-            size * rows
-        }&fit=crop&auto=format&dpr=2 2x`,
-    };
-}
-
 const Images = () => {
     return (
         <ImageList
-            variant="quilted"
             cols={3}
             sx={{ mb: 2 }}
         >
