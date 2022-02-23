17d75bcddc1f31d288c419bb7e28039394bab4d4
Jean-Paul <jpzambrano.leon@gmail.com>
2022-02-23 00:26:36 -0600

    updated Images.jsx

--- src/pages/profilePage/components/Images.jsx
+++ src/pages/profilePage/components/Images.jsx
@@ -24,9 +24,6 @@ const Images = () => {
                     />
                 </ImageListItem>
             ))}
-            <ImageListItem>
-                <img src="https://images.unsplash.com/photo-1551963831-b3b1ca40c98e?w=242&h=242&fit=crop&auto=format&dpr=2" alt="" />
-            </ImageListItem>
         </ImageList>
     );
 };
@@ -41,7 +38,11 @@ const itemData = [
     {
         img: 'https://images.unsplash.com/photo-1551782450-a2132b4ba21d',
         title: 'Burger',
-    }
+    },
+    {
+        img: 'https://images.unsplash.com/photo-1522770179533-24471fcdba45',
+        title: 'Camera',
+    },
 ]
 
 export default Images;
