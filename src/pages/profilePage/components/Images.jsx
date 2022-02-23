33832a2c7aadc0d7bf33c46ad4fe225139d43dca
Jean-Paul <jpzambrano.leon@gmail.com>
2022-02-22 22:32:27 -0600

    updated Images.jsx

--- src/pages/profilePage/components/Images.jsx
+++ src/pages/profilePage/components/Images.jsx
@@ -2,7 +2,10 @@ import { ImageList, ImageListItem } from "@mui/material";
 
 const Images = () => {
     return (
-        <ImageList>
+        <ImageList
+            variant="quilted"
+            
+        >
             <ImageListItem>
                 <img src="https://images.unsplash.com/photo-1551963831-b3b1ca40c98e?w=242&h=242&fit=crop&auto=format&dpr=2" alt="" />
             </ImageListItem>
