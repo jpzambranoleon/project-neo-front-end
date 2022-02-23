1c19432607a8dd870981392592fe32f58655a0dc
Jean-Paul <jpzambrano.leon@gmail.com>
2022-02-23 00:08:08 -0600

    updated Images.jsx

--- src/pages/profilePage/components/Images.jsx
+++ src/pages/profilePage/components/Images.jsx
@@ -16,9 +16,9 @@ const Images = () => {
             cols={3}
         >
             {itemData.map((item) => (
-                <ImageList>
-                    
-                </ImageList>
+                <ImageListItem>
+
+                </ImageListItem>
             ))}
             <ImageListItem>
                 <img src="https://images.unsplash.com/photo-1551963831-b3b1ca40c98e?w=242&h=242&fit=crop&auto=format&dpr=2" alt="" />
