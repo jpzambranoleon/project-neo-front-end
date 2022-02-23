305586fefe3c7ed34a6ad1b6839c535181deb53f
Jean-Paul <jpzambrano.leon@gmail.com>
2022-02-22 18:09:25 -0600

    updated ImageList.jsx

--- src/pages/profilePage/components/ImageList.jsx
+++ src/pages/profilePage/components/ImageList.jsx
@@ -1,8 +1,10 @@
+import { ImageListItem } from "@mui/material";
+
 const ImageList = () => {
     return (
-        <Box>
-            Hello
-        </Box>
+        <ImageListItem>
+            <img src="https://images.unsplash.com/photo-1551963831-b3b1ca40c98e?w=242&h=242&fit=crop&auto=format&dpr=2" alt="" />
+        </ImageListItem>
     );
 };
 
