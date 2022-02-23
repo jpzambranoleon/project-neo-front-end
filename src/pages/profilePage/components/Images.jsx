317bea8109dba77a63fe572591a4f089cb972f47
Jean-Paul <jpzambrano.leon@gmail.com>
2022-02-22 23:32:52 -0600

    updated Images.jsx

--- src/pages/profilePage/components/Images.jsx
+++ src/pages/profilePage/components/Images.jsx
@@ -15,6 +15,11 @@ const Images = () => {
             variant="quilted"
             cols={3}
         >
+            {itemData.map((item) => (
+                <ImageList>
+                    
+                </ImageList>
+            ))}
             <ImageListItem>
                 <img src="https://images.unsplash.com/photo-1551963831-b3b1ca40c98e?w=242&h=242&fit=crop&auto=format&dpr=2" alt="" />
             </ImageListItem>
