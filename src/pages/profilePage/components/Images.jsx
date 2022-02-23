fefb6a8b0b496d173c31e18789264ba1fddee888
Jean-Paul <jpzambrano.leon@gmail.com>
2022-02-22 23:31:06 -0600

    updated Images.jsx

--- src/pages/profilePage/components/Images.jsx
+++ src/pages/profilePage/components/Images.jsx
@@ -1,6 +1,13 @@
 import { ImageList, ImageListItem } from "@mui/material";
 
-function srcset()
+function srcset(image, size, rows = 1, cols = 1) {
+    return {
+        src: '${image}?w=${size * cols}&h=${size * rows}&fit=crop&auto=format',
+        srcSet: `${image}?w=${size * cols}&h=${
+            size * rows
+        }&fit=crop&auto=format&dpr=2 2x`,
+    };
+}
 
 const Images = () => {
     return (
