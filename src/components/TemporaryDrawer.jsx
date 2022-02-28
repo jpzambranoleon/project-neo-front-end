c7c8764f412a418406d2b895226c427f953be76a
Jean-Paul <jpzambrano.leon@gmail.com>
2022-02-28 17:19:36 -0600

    updated TemporaryDrawer.jsx

--- src/components/TemporaryDrawer.jsx
+++ src/components/TemporaryDrawer.jsx
@@ -1,39 +1,20 @@
-import { Inbox } from "@mui/icons-material";
-import { Box, List, ListItem, ListItemButton, ListItemIcon, ListItemText } from "@mui/material";
+import { Home, Inbox } from "@mui/icons-material";
+import { Box, Button, Drawer, List, ListItem, ListItemButton, ListItemIcon, ListItemText } from "@mui/material";
 import { useState } from "react";
 
 
 const TemporaryDrawer = () => {
-    const [state, setState] = useState({
-        left: false
-    });
 
-    const toggleDrawer = (anchor, open) => (event) => {
-        if (event.type === 'keydown' && (event.key === 'Tab' || event.key === 'Shift' )) {
-            return;
-        }
-
-        setState({ ...state, [anchor]: open })
-    };
-
-    const list = (anchor) => (
-        <Box
-            sx={{ width: anchor === 'top' || anchor === 'bottom' ? 'auto' : 250 }}
-            role="presentation"
-            onClick={toggleDrawer(anchor, false)}
-            onKeyDown={toggleDrawer(anchor, false)}
-        >
+    return (
+        <Drawer>
             <List>
-                {['Home', 'Profile', 'Messages', 'Explore', 'Courses', 'Projects', 'Bookmarks', 'Settings'].map((text, index) => (
-                    <ListItem button key={text}>
-                        <ListItemIcon>
-                            {index % 2 === 0 ? <Inbox /> : <Mail />}
-                        </ListItemIcon>
-                        <ListItemText primary={text} />
-                    </ListItem>
-                ))}
+                <ListItemButton>
+                    <ListItemIcon>
+                        <Home />
+                    </ListItemIcon>
+                </ListItemButton>
             </List>
-        </Box>
+        </Drawer>
     )
 }
 
