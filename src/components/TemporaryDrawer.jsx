44d00e8638a0a1a65ea07203a8e981130a7b2ff9
Jean-Paul <jpzambrano.leon@gmail.com>
2022-02-28 17:42:05 -0600

    updated TemporaryDrawer.jsx

--- src/components/TemporaryDrawer.jsx
+++ src/components/TemporaryDrawer.jsx
@@ -1,4 +1,4 @@
-import { Home, Inbox } from "@mui/icons-material";
+import { Apps, Bookmark, Explore, Home, Inbox, LaptopChromebook, Message, Person, Settings } from "@mui/icons-material";
 import { Box, Button, Drawer, List, ListItem, ListItemButton, ListItemIcon, ListItemText } from "@mui/material";
 import { useState } from "react";
 
@@ -7,13 +7,72 @@ const TemporaryDrawer = () => {
 
     return (
         <Drawer open={true}>
-            <List>
+            <Box sx={{ width: 250 }}>
                 <ListItemButton>
                     <ListItemIcon>
                         <Home />
                     </ListItemIcon>
+                    <ListItemText>
+                        Home
+                    </ListItemText>
                 </ListItemButton>
-            </List>
+                <ListItemButton>
+                    <ListItemIcon>
+                        <Person />
+                    </ListItemIcon>
+                    <ListItemText>
+                        Profile
+                    </ListItemText>
+                </ListItemButton>
+                <ListItemButton>
+                    <ListItemIcon>
+                        <Message />
+                    </ListItemIcon>
+                    <ListItemText>
+                        Messages
+                    </ListItemText>
+                </ListItemButton>
+                <ListItemButton>
+                    <ListItemIcon>
+                        <Explore />
+                    </ListItemIcon>
+                    <ListItemText>
+                        Explore
+                    </ListItemText>
+                </ListItemButton>
+                <ListItemButton>
+                    <ListItemIcon>
+                        <LaptopChromebook />
+                    </ListItemIcon>
+                    <ListItemText>
+                        Courses
+                    </ListItemText>
+                </ListItemButton>
+                <ListItemButton>
+                    <ListItemIcon>
+                        <Apps />
+                    </ListItemIcon>
+                    <ListItemText>
+                        Projects
+                    </ListItemText>
+                </ListItemButton>
+                <ListItemButton>
+                    <ListItemIcon>
+                        <Bookmark />
+                    </ListItemIcon>
+                    <ListItemText>
+                        Bookmarks
+                    </ListItemText>
+                </ListItemButton>
+                <ListItemButton>
+                    <ListItemIcon>
+                        <Settings />
+                    </ListItemIcon>
+                    <ListItemText>
+                        Settings
+                    </ListItemText>
+                </ListItemButton>
+            </Box>
         </Drawer>
     )
 }
