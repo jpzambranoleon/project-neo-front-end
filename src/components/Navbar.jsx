f3a7389648f4ff92c6fd6b13168ce7acb392615c
Jean-Paul <jpzambrano.leon@gmail.com>
2022-02-27 21:11:02 -0600

    updated Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -37,10 +37,7 @@ const StyledInputBase = styled(InputBase)(({ theme }) => ({
         transition: theme.transitions.create('width'),
         width: '100%',
         [theme.breakpoints.up('md')]: {
-            width: '12ch',
-            '&:focus': {
-                width: '20ch',
-            },
+            width: '20ch',
         },
     },
 }));
