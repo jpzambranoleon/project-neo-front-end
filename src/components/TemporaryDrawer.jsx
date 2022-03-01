7a58ded56a40e66d07644ab7c18fdc4f5fc3c835
Jean-Paul <jpzambrano.leon@gmail.com>
2022-02-28 19:31:07 -0600

    updated files

--- src/components/TemporaryDrawer.jsx
+++ src/components/TemporaryDrawer.jsx
@@ -4,91 +4,90 @@ import { useState } from "react";
 
 
 const TemporaryDrawer = ({ openDrawer, setOpenDrawer }) => {
-    const [open, setOpen] = useState(false);
 
-    const handleClickAway = () => {
-        setOpen(false);
+    const handleDrawerClose = () => {
+        setOpenDrawer(false);
     };
 
     return (
-        <ClickAwayListener onClickAway={handleClickAway}>
         <Drawer anchor="left" open={openDrawer} sx={{ display: { sm: 'none' } }} >
-            <Box sx={{ width: 250 }}>
-                <Container sx={{ display: 'flex', alignItems: 'center', justifyContent: 'space-between' }}>
-                    <Typography variant="h6">
-                        Menu
-                    </Typography>
-                    <IconButton>
-                        <Close />
-                    </IconButton>
-                </Container>
-                <ListItemButton>
-                    <ListItemIcon>
-                        <Home />
-                    </ListItemIcon>
-                    <ListItemText>
-                        Home
-                    </ListItemText>
-                </ListItemButton>
-                <ListItemButton>
-                    <ListItemIcon>
-                        <Person />
-                    </ListItemIcon>
-                    <ListItemText>
-                        Profile
-                    </ListItemText>
-                </ListItemButton>
-                <ListItemButton>
-                    <ListItemIcon>
-                        <Message />
-                    </ListItemIcon>
-                    <ListItemText>
-                        Messages
-                    </ListItemText>
-                </ListItemButton>
-                <ListItemButton>
-                    <ListItemIcon>
-                        <Explore />
-                    </ListItemIcon>
-                    <ListItemText>
-                        Explore
-                    </ListItemText>
-                </ListItemButton>
-                <ListItemButton>
-                    <ListItemIcon>
-                        <LaptopChromebook />
-                    </ListItemIcon>
-                    <ListItemText>
-                        Courses
-                    </ListItemText>
-                </ListItemButton>
-                <ListItemButton>
-                    <ListItemIcon>
-                        <Apps />
-                    </ListItemIcon>
-                    <ListItemText>
-                        Projects
-                    </ListItemText>
-                </ListItemButton>
-                <ListItemButton>
-                    <ListItemIcon>
-                        <Bookmark />
-                    </ListItemIcon>
-                    <ListItemText>
-                        Bookmarks
-                    </ListItemText>
-                </ListItemButton>
-                <ListItemButton>
-                    <ListItemIcon>
-                        <Settings />
-                    </ListItemIcon>
-                    <ListItemText>
-                        Settings
-                    </ListItemText>
-                </ListItemButton>
-            </Box>
+            <ClickAwayListener onClickAway={handleDrawerClose}>
+                <Box sx={{ width: 250 }}>
+                    <Container sx={{ display: 'flex', alignItems: 'center', justifyContent: 'space-between' }}>
+                        <Typography variant="h6">
+                            Menu
+                        </Typography>
+                        <IconButton onClick={handleDrawerClose}>
+                            <Close />
+                        </IconButton>
+                    </Container>
+                    <ListItemButton>
+                        <ListItemIcon>
+                            <Home />
+                        </ListItemIcon>
+                        <ListItemText>
+                            Home
+                        </ListItemText>
+                    </ListItemButton>
+                    <ListItemButton>
+                        <ListItemIcon>
+                            <Person />
+                        </ListItemIcon>
+                        <ListItemText>
+                            Profile
+                        </ListItemText>
+                    </ListItemButton>
+                    <ListItemButton>
+                        <ListItemIcon>
+                            <Message />
+                        </ListItemIcon>
+                        <ListItemText>
+                            Messages
+                        </ListItemText>
+                    </ListItemButton>
+                    <ListItemButton>
+                        <ListItemIcon>
+                            <Explore />
+                        </ListItemIcon>
+                        <ListItemText>
+                            Explore
+                        </ListItemText>
+                    </ListItemButton>
+                    <ListItemButton>
+                        <ListItemIcon>
+                            <LaptopChromebook />
+                        </ListItemIcon>
+                        <ListItemText>
+                            Courses
+                        </ListItemText>
+                    </ListItemButton>
+                    <ListItemButton>
+                        <ListItemIcon>
+                            <Apps />
+                        </ListItemIcon>
+                        <ListItemText>
+                            Projects
+                        </ListItemText>
+                    </ListItemButton>
+                    <ListItemButton>
+                        <ListItemIcon>
+                            <Bookmark />
+                        </ListItemIcon>
+                        <ListItemText>
+                            Bookmarks
+                        </ListItemText>
+                    </ListItemButton>
+                    <ListItemButton>
+                        <ListItemIcon>
+                            <Settings />
+                        </ListItemIcon>
+                        <ListItemText>
+                            Settings
+                        </ListItemText>
+                    </ListItemButton>
+                </Box>
+            </ClickAwayListener>
         </Drawer>
-        </ClickAwayListener>
     )
 }
 
