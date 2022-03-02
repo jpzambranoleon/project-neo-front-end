6bf6259f1daf68f876731bc49527f57139b9f66d
Jean-Paul <jpzambrano.leon@gmail.com>
2022-03-01 22:52:36 -0600

    updated Add.jsx

--- src/pages/homePage/components/Add.jsx
+++ src/pages/homePage/components/Add.jsx
@@ -1,15 +1,21 @@
 import { Edit } from "@mui/icons-material";
-import { Fab, Tooltip } from "@mui/material"
+import { Fab, styled, Tooltip } from "@mui/material"
 import { useState } from "react"
+
+const CustomFab = styled(Fab)(({ theme }) => ({
+    position: 'fixed',
+    bottom: 20,
+    right: 20,
+}));
 
 const Add = () => {
     const [open, setOpen] = useState(false);
     const [openAlert, setOpenAlert] = useState(false);
     return (
         <Tooltip title="Create" onClick={() => setOpen(true)}>
-            <Fab color="secondary" aria-label="create">
+            <CustomFab color="secondary" aria-label="create">
                 <Edit />
-            </Fab>
+            </CustomFab>
         </Tooltip>
     );
 };
