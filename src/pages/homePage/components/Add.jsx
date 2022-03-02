4494dc56b3655054940bdbe4d58adf6ab67278d9
Jean-Paul <jpzambrano.leon@gmail.com>
2022-03-01 22:40:58 -0600

    updated Add.jsx

--- src/pages/homePage/components/Add.jsx
+++ src/pages/homePage/components/Add.jsx
@@ -1,10 +1,14 @@
+import { Edit } from "@mui/icons-material";
 import { Fab, Tooltip } from "@mui/material"
+import { useState } from "react"
 
 const Add = () => {
+    const [open, setOpen] = useState(false);
+    const [openAlert, setOpenAlert] = useState(false);
     return (
         <Tooltip>
-            <Fab>
-                
+            <Fab color="secondary" aria-label="create">
+                <Edit />
             </Fab>
         </Tooltip>
     )
