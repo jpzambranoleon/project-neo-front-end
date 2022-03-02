a03184e72d56bcdf76cfba5644c13d78e2b1bcc9
Jean-Paul <jpzambrano.leon@gmail.com>
2022-03-01 22:38:33 -0600

    updated Add.jsx

---
+++ src/pages/homePage/components/Add.jsx
@@ -0,0 +1,11 @@
+import { Fab, Tooltip } from "@mui/material"
+
+const Add = () => {
+    return (
+        <Tooltip>
+            <Fab>
+                
+            </Fab>
+        </Tooltip>
+    )
+}
