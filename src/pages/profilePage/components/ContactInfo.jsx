01cb2a0bdb8d8f338c8eef0464731638c8f35080
Jean-Paul <jpzambrano.leon@gmail.com>
2022-03-11 17:28:11 -0600

    updated ContactInfo.jsx

---
+++ src/pages/profilePage/components/ContactInfo.jsx
@@ -0,0 +1,11 @@
+import { Button, Modal } from "@mui/material";
+
+const ContactInfo = () => {
+    return (
+        <Button sx={{ mt: 2, fontSize: 10, textTransform: 'none' }} variant="contained" size="small">
+            Contact Info
+        </Button>
+    )
+};
+
+export default ContactInfo;
