03131ec567749b663249c25e144be6d7c38d4f7d
Jean-Paul <jpzambrano.leon@gmail.com>
2022-03-11 17:42:50 -0600

    updated ContactInfo.jsx

--- src/pages/profilePage/components/ContactInfo.jsx
+++ src/pages/profilePage/components/ContactInfo.jsx
@@ -1,10 +1,44 @@
-import { Button, Modal } from "@mui/material";
+import { Cancel } from "@mui/icons-material";
+import { Box, Button, Container, IconButton, Modal, styled, Typography } from "@mui/material";
+import { useState } from "react";
+
+const CustomContainer = styled(Container)(({ theme }) => ({
+    width: 500,
+    height: 550,
+    backgroundColor: "white",
+    position: "absolute",
+    top: 65,
+    left: 0,
+    right: 0,
+    margin: "auto",
+    [theme.breakpoints.down("sm")]: {
+        top: 0,
+        width: "100%",
+        height: "100%"
+    }
+}))
 
 const ContactInfo = () => {
+    const [open, setOpen] = useState(false);
+
     return (
-        <Button sx={{ mt: 2, fontSize: 10, textTransform: 'none' }} variant="contained" size="small">
-            Contact Info
-        </Button>
+        <>
+            <Button onClick={() => setOpen(true)} sx={{ mt: 2, fontSize: 10, textTransform: 'none' }} variant="contained" size="small">
+                Contact Info
+            </Button>
+            <Modal open={open}>
+                <CustomContainer>
+                    <Box sx={{ mt: 2, display: 'flex', alignItems: 'center', justifyContent: 'space-between' }}>
+                        <Typography>
+                            Edit Profile
+                        </Typography>
+                        <IconButton onClick={() => setOpen(false)}>
+                            <Cancel />
+                        </IconButton>
+                    </Box>
+                </CustomContainer>
+            </Modal>
+        </>
     )
 };
 
