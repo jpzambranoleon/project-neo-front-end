9eadac5d59e53b44b8b46591689d80f367c4c2cf
Jean-Paul <jpzambrano.leon@gmail.com>
2022-05-13 19:30:56 -0500

    updated Home.jsx

--- src/pages/homePage/Home.jsx
+++ src/pages/homePage/Home.jsx
@@ -1,11 +1,7 @@
-import { Box, Grid, styled } from "@mui/material";
-import Navbar from "../../components/Navbar";
+import { Box, Grid } from "@mui/material";
 import Feed from "./components/Feed";
 import Post from "./components/Post";
-import Sidebar from "../../components/Sidebar";
-import User from "../../components/User";
-import News from "../../components/News";
-import Add from "./components/Add";
+
 
 export default function Home() {
   return (
