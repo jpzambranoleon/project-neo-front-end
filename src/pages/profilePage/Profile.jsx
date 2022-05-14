41d690477b4ceb3c4ffd099b9dd58257cc9ef00e
Jean-Paul <jpzambrano.leon@gmail.com>
2022-05-13 19:29:25 -0500

    updated Profile.jsx

--- src/pages/profilePage/Profile.jsx
+++ src/pages/profilePage/Profile.jsx
@@ -1,20 +1,6 @@
-import { Box, Grid, styled } from "@mui/material";
-import Navbar from "../../components/Navbar";
+import { Grid } from "@mui/material";
 import Description from "./components/Description";
-import Sidebar from "../../components/Sidebar";
-import User from "../../components/User";
-import Images from "./components/Images";
-import News from "../../components/News";
 
-const CustomBox = styled(Box)(({ theme }) => ({
-  width: "80%",
-  margin: "0 auto",
-  marginTop: theme.spacing(10),
-  [theme.breakpoints.down("sm")]: {
-    width: "100%",
-    marginTop: theme.spacing(4),
-  },
-}));
 
 export default function Profile() {
   return (
