0f9cd037819996babc01536145605535c63c32cf
Jean-Paul <jpzambrano.leon@gmail.com>
2022-06-25 00:46:57 -0500

    updated files

--- src/pages/Explore/Explore.jsx
+++ src/pages/Explore/Explore.jsx
@@ -1,14 +1,13 @@
-import { Box, Container, Grid, Paper, Typography,} from "@mui/material";
+import { Box, Container, Grid, Paper, } from "@mui/material";
 import SearchBar from "./components/SearchBar";
 import UserCard from "./components/UserCard";
-import Users from "./components/Users";
 
 export default function Explore() {
   return (
     <>
         <Grid item sm={6}>
-            <SearchBar />
             <Paper>
+            <SearchBar />
               <Container>
                 <Grid container spacing={2}>
                   <Grid item xs={12} sm={6}>
--- src/pages/Explore/components/SearchBar.jsx
+++ src/pages/Explore/components/SearchBar.jsx
@@ -1,25 +1,22 @@
 import { Directions, Menu, Search } from "@mui/icons-material";
-import { Divider, IconButton, InputBase, Paper } from "@mui/material";
+import { Box, Divider, IconButton, InputBase, Paper } from "@mui/material";
 
 const SearchBar = () => {
     return (
-        <Paper component="form" sx={{ p: '2px 4px', display: 'flex', alignItems: 'center', mb: 2 }}>
-              <IconButton>
-                <Menu />
-              </IconButton>
-              <InputBase
-                sx={{ ml: 1, flex: 1 }}
-                placeholder="Search Users"
-                inputProps={{ 'aria-label': 'search users' }}
-              />
-              <IconButton type="submit" sx={{ p: '10px' }} aria-label="search">
-                <Search />
-              </IconButton>
-              <Divider sx={{ height: 28, m: 0.5 }} orientation="vertical" />
-              <IconButton color="primary" sx={{ p: '10px' }} aria-label="directions">
-                <Directions />
-              </IconButton>
-        </Paper>
+      <Box component="form" sx={{ p: '2px 4px', display: 'flex', alignItems: 'center', mb: 2 }}>
+        <IconButton>
+          <Menu />
+        </IconButton>
+        <InputBase
+          sx={{ ml: 1, flex: 1 }}
+          placeholder="Search Users"
+          inputProps={{ 'aria-label': 'search users' }}
+        />
+        <Divider sx={{ height: 28, m: 0.5 }} orientation="vertical" />
+        <IconButton color="primary" sx={{ p: '10px' }} aria-label="search">
+          <Search />
+        </IconButton>
+      </Box>
     );
 };
 
