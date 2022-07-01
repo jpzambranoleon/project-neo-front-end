fefcc6832dd69be19be8b816dc9a2d479df76ba1
Jean-Paul <jpzambrano.leon@gmail.com>
2022-06-30 21:31:07 -0500

    added icon buttons to modal

--- src/components/CreatePostButton.jsx
+++ src/components/CreatePostButton.jsx
@@ -1,4 +1,5 @@
-import { Avatar, Box, Button, Modal, styled, Typography, } from "@mui/material";
+import { DateRange, EmojiEmotions, Image, PersonAdd, VideoCameraBack } from "@mui/icons-material";
+import { Avatar, Box, Button, ButtonGroup, IconButton, Modal, Stack, styled, TextField, Typography, } from "@mui/material";
 import { useState } from "react";
 
 const StyledModal = styled(Modal)({
@@ -40,9 +41,44 @@ const CreatePostButton = () => {
                         Create Post
                     </Typography>
                     <UserBox>
-                        <Avatar src="/assets/person/diana.jpg" />
+                        <Avatar 
+                            src="/assets/person/diana.jpg" 
+                            sx={{ width: 30, height: 30 }}    
+                        />
                         <Typography fontWeight={500} variant="span">Diana Ayi</Typography>
                     </UserBox>
+                    <TextField 
+                        id="standard-multiline-static"
+                        multiline
+                        fullWidth
+                        rows={3}
+                        placeholder="What's on your mind?"
+                        variant="standard"
+                    />
+                    <Stack direction="row" gap={1} mt={2} mb={3}>
+                        <IconButton aria-label="add emoji" color="primary">
+                            <EmojiEmotions color="primary" />
+                        </IconButton>
+                        <IconButton aria-label="add image" color="secondary">
+                            <Image color="secondary" />
+                        </IconButton>
+                        <IconButton aria-label="add video" color="success">
+                            <VideoCameraBack color="success" />
+                        </IconButton>
+                        <IconButton aria-label="add person" color="error">
+                            <PersonAdd color="error" />
+                        </IconButton>
+                    </Stack>
+                    <ButtonGroup
+                        fullWidth 
+                        variant="contained"
+                        aria-label="contained primary button group"
+                    >
+                        <Button>Post</Button>
+                        <Button sx={{ width: '100px' }}>
+                            <DateRange />
+                        </Button>
+                    </ButtonGroup>
                 </Box>
             </StyledModal>
         </div>
