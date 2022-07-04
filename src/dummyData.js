59145dbf75ce3284159241a366a9c194cdffc376
Jean-Paul <jpzambrano.leon@gmail.com>
2022-07-04 13:48:29 -0500

    created dummy data for props

---
+++ src/dummyData.js
@@ -0,0 +1,58 @@
+export const Users = [
+    {
+        id: 1,
+        profilePicture: "https://material-ui.com/static/images/avatar/1.jpg",
+        username: "Remy Sharp",
+    },
+    {
+        id: 2,
+        profilePicture: "/assets/person/diana.jpg",
+        username: "Diana Ayi"
+    },
+    {
+        id: 3,
+        profilePicture: "https://material-ui.com/static/images/avatar/2.jpg",
+        userName: "Travis Howard"
+    },
+    {
+        id: 4,
+        profilePicture: "https://material-ui.com/static/images/avatar/3.jpg",
+        userName: "Cindy Baker"
+    }
+];
+
+export const Posts = [
+    {
+        id: 1,
+        desc: "Love for All, Hatred for None",
+        photo: "",
+        date: "5 mins ago",
+        userId: 1,
+        like: 32,
+        comment: 9,
+    },
+    {
+        id: 2,
+        photo: "",
+        date: "15 mins ago",
+        userId: 2,
+        like: 2,
+        comment: 1,
+    },
+    {
+        id: 3,
+        photo: "",
+        date: "1 hour ago",
+        userId: 3,
+        like: 14,
+        comment: 4,
+    },
+    {
+        id: 4,
+        photo: "",
+        date: "3 hours ago",
+        userId: 4,
+        like: 65,
+        comment: 0
+    }
+]
